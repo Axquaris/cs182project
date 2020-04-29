4991cdeb55a8d9d1ab3552a0e6f7ff3665debec5
Omar Khan <o.k@berkeley.edu>
2020-04-29 15:31:46 -0700

    created script to modify baseline model and write to disc

--- CS182-Spring2020-Vision-Project-PyTorch/models/baseline_resnet.py
+++ CS182-Spring2020-Vision-Project-PyTorch/models/baseline_resnet.py
@@ -1,8 +1,30 @@
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 import torch
 import torchvision
 
+# How many layers (out of 10) will NOT be updated
+NUM_FROZEN_LAYERS = 6
+# How many logits (classes) to have in the last layer
+NUM_LOGITS = 10000
+# The input number to the last layer (found by printing the model)
+NUM_INPUT = 2048
+# The path this baseline model will be saved to
+SAVE_PATH = "./base_model_modified"
+
 base_model = torchvision.models.resnet50(pretrained=True)
 
-for name, child in base_model.named_children():
-    for name2, params in child.named_parameters():
-        print(name, name2)
+# Freezing layers
+child_num = 1
+for child in base_model.children():
+    if child_num < NUM_FROZEN_LAYERS:
+        for param in child.parameters():
+            param.requires_grad = False
+    child_num += 1
+
+# Updating number of logits in last layer
+base_model.fc = torch.nn.Linear(NUM_INPUT, NUM_LOGITS)
+
+# Writing model to disc
+torch.save(base_model.state_dict(), SAVE_PATH)
