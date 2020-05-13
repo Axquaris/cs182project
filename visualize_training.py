00443d00ca429bd294bb5870926325c72d11e10c
Nathan Miller <nathan_miller23@berkeley.edu>
2020-05-13 19:37:14 -0400

    added gan training visualization

--- visualize_training.py
+++ visualize_training.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import torch, sys
+import argparse
+import numpy as np
 
-if __name__ == "__main__":
-    assert len(sys.argv) == 2, "Please specify path to model checkpoint"
-    chkpt_path = sys.argv[1]
-    checkpoint = torch.load(chkpt_path, map_location=torch.device('cpu'))
+
+def visualize_clf(checkpoint):
     t_losses = checkpoint['t_loss']
     t_accuracies = checkpoint['t_acc']
     v_losses = checkpoint['v_loss']
@@ -21,9 +21,36 @@ if __name__ == "__main__":
     ax4.plot(v_accuracies)
 
     fig.subplots_adjust(hspace=1.0)
-    plt.show()
 
-    # print("v_losses", v_losses)
-    # print("t_losses", t_losses)
-    # print("v_accuracies", v_accuracies)
-    # print("t_accuracies", t_accuracies)
+def visualize_gan(checkpoint):
+    dis_losses = checkpoint['dis_loss']
+    gen_losses = checkpoint['gen_loss']
+
+    adv_losses = [gen_loss['adversarial_loss'] for gen_loss in gen_losses]
+    hinge_losses = [gen_loss['hinge_loss'] for gen_loss in gen_losses]
+    fooling_losses = [gen_loss['fooling_loss'] for gen_loss in gen_losses]
+
+    num_iters = len(adv_losses)
+
+    x = np.linspace(0, num_iters, num_iters)
+    plt.plot(x, dis_losses, label="Discriminator Loss")
+    plt.plot(x, adv_losses, label="Adversarial Loss")
+    plt.plot(x, fooling_losses, label="Fooling Loss")
+    plt.plot(x, hinge_losses, label="Pertubation Regularization Loss")
+
+    plt.legend()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--gan", action="store_true")
+    parser.add_argument("-p", "--checkpoint-path", type=str)
+    args = parser.parse_args()
+    chkpt_path = args.checkpoint_path
+    checkpoint = torch.load(chkpt_path, map_location=torch.device('cpu'))
+
+    if args.gan:
+        visualize_gan(checkpoint)
+    else:
+        visualize_clf(checkpoint)
+    
+    plt.show()
